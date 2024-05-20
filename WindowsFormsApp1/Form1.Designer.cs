21be5b98b07dd4750dbdf8b7ee8cab2f9717cd94
Linga <Linga@DESKTOP-FOVO1OM>
2024-05-20 22:31:01 +0400

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtPDFFileName = new System.Windows.Forms.TextBox();
+            this.txtXMLFilename = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.lblresponse = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtPDFFileName
+            // 
+            this.txtPDFFileName.Location = new System.Drawing.Point(168, 69);
+            this.txtPDFFileName.Name = "txtPDFFileName";
+            this.txtPDFFileName.ReadOnly = true;
+            this.txtPDFFileName.Size = new System.Drawing.Size(485, 20);
+            this.txtPDFFileName.TabIndex = 0;
+            // 
+            // txtXMLFilename
+            // 
+            this.txtXMLFilename.Location = new System.Drawing.Point(168, 95);
+            this.txtXMLFilename.Name = "txtXMLFilename";
+            this.txtXMLFilename.ReadOnly = true;
+            this.txtXMLFilename.Size = new System.Drawing.Size(485, 20);
+            this.txtXMLFilename.TabIndex = 0;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(168, 121);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(82, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Attach XML";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(93, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "PDF File:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(93, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "XML File:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(280, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(217, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Convert PDF to PDF/A";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(659, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(23, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(659, 97);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(23, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Location = new System.Drawing.Point(168, 144);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(285, 70);
+            this.btnConvert.TabIndex = 5;
+            this.btnConvert.Text = "Convert ";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // lblresponse
+            // 
+            this.lblresponse.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.lblresponse.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresponse.Location = new System.Drawing.Point(9, 263);
+            this.lblresponse.Name = "lblresponse";
+            this.lblresponse.Size = new System.Drawing.Size(781, 23);
+            this.lblresponse.TabIndex = 6;
+            this.lblresponse.Text = "e-Technologies@2024";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(106, 40);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Red;
+            this.button3.Location = new System.Drawing.Point(465, 144);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(188, 70);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Close";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 295);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblresponse);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.txtXMLFilename);
+            this.Controls.Add(this.txtPDFFileName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Convert PDF to PDF/A ";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPDFFileName;
+        private System.Windows.Forms.TextBox txtXMLFilename;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label lblresponse;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string BASE_URL = @"https://epdf.mylstech.com/api/";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnConvert_Click(object sender, EventArgs e)
+        {
+            //1. Declare Variable
+            string saveInvoiceName = "Inv-" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".pdf",
+                    pdfFileName, xmlFilename;
+            byte[] pdfBytes = null,xmlBytes= null, responseContentBytes=null;
+
+            HttpResponseMessage response = null;
+            MultipartFormDataContent form = new MultipartFormDataContent();
+
+            //2. Convert File to Bytes Array
+            pdfFileName =txtPDFFileName.Text;
+            xmlFilename = txtXMLFilename.Text;
+            if(string.IsNullOrEmpty(pdfFileName)  )
+            {
+                return;
+            }
+            using(FileStream stream = File.Open(pdfFileName,FileMode.Open) )
+            {
+                pdfBytes= new byte[stream.Length];
+                await stream.ReadAsync(pdfBytes, 0, (int)stream.Length);
+            }
+            var pdffileContent = new ByteArrayContent(pdfBytes);
+            pdffileContent.Headers.ContentType = MediaTypeHeaderValue.Parse("application/pdf");
+            form.Add(pdffileContent, "pdfFile", Path.GetFileName(pdfFileName));   //3. Construct Mulform Data
+
+
+
+            if (!string.IsNullOrEmpty(xmlFilename) && checkBox1.Checked)
+            {
+                using (FileStream stream = File.Open(xmlFilename, FileMode.Open))
+                {
+                    xmlBytes = new byte[stream.Length];
+                    await stream.ReadAsync(xmlBytes, 0, (int)stream.Length);
+                }
+                var xmlfileContent = new ByteArrayContent(xmlBytes);
+                xmlfileContent.Headers.ContentType = MediaTypeHeaderValue.Parse("application/xml");
+                form.Add(xmlfileContent, "xmlFile", Path.GetFileName(xmlFilename));   //3. Construct Mulform Data
+            }
+           
+             
+            
+            // here it is important that second parameter matches with name given in API.
+
+            //MultipartFormDataContent form = new MultipartFormDataContent()
+            //{
+            //    {pdffileContent,"pdfFile",Path.GetFileName(pdfFileName)  }     ,
+            //    {xmlfileContent,"xmlFile",Path.GetFileName(pdfFileName) }
+
+            //};
+
+            
+
+
+
+            //4. Send To Service
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.ConnectionClose = true;
+                httpClient.BaseAddress = new Uri(BASE_URL);// "https://localhost:44314/api/");
+                response = await httpClient.PostAsync($"ZatcaService/ConvertPDFtoPDFA3", form);
+                response.EnsureSuccessStatusCode();
+                responseContentBytes = await response.Content.ReadAsByteArrayAsync();
+            };
+            if (responseContentBytes == null)
+            {
+                MessageBox.Show("Error File Creataion");
+                return;
+            }
+            //5. Save the Response
+
+            try
+            {
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                saveFileDialog1.Filter = "pdf files (*.pdf)|*.pdf";
+                saveFileDialog1.FileName = saveInvoiceName;// "InvoicePDFA3.pdf";
+                DialogResult resultdiag = saveFileDialog1.ShowDialog();
+
+                if (resultdiag == DialogResult.OK)
+                {
+                    File.WriteAllBytes(saveFileDialog1.FileName, responseContentBytes);
+                    MessageBox.Show("File Saved Successfuly at " + saveFileDialog1.FileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.InnerException.ToString());
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog()
+            {
+                InitialDirectory = @"C:\Users\Linga\Desktop\PDFA\",
+                Title = "Browse PDF Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "pdf",
+                Filter = "Pdf files (*.pdf)|*.pdf",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtPDFFileName.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog()
+            {
+                InitialDirectory = @"C:\Users\Linga\Desktop\PDFA\",
+                Title = "Browse XML Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "xml",
+                Filter = "xml files (*.xml)|*.xml",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtXMLFilename.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
